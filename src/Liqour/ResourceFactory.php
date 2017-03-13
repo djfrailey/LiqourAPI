f18bc38af2a9091b2a9d9c9c5f2e382ef6280e0a
David Frailey <david@snapshot.is>
2017-03-12 22:05:15 -0700

    More semantic variable names

--- src/Liqour/ResourceFactory.php
+++ src/Liqour/ResourceFactory.php
@@ -11,97 +11,97 @@ class ResourceFactory
         $this->api = $api;
     }
 
-    public function createPriceBag(array $contentBody) : Bag
+    public function createPriceBag(array $prices) : Bag
     {
-        $prices = new Bag();
+        $priceBag = new Bag();
         
-        foreach($contentBody as $rawPrice) {
+        foreach($prices as $rawPrice) {
             $price = $this->createPrice($rawPrice);
-            $prices->set((string) $price->getId(), $price);
+            $priceBag->set((string) $price->getId(), $price);
         }
 
-        return $prices;
+        return $priceBag;
     }
 
-    public function createPrice(stdClass $contentBody) : Price
+    public function createPrice(stdClass $price) : Price
     {
-        $createdAt = new DateTime($contentBody->created_at);
-        $modifiedAt = new DateTime($contentBody->modified_at);
+        $createdAt = new DateTime($price->created_at);
+        $modifiedAt = new DateTime($price->modified_at);
 
         return new Price(
-            $contentBody->id,
-            $contentBody->amount,
+            $price->id,
+            $price->amount,
             $createdAt,
             $modifiedAt,
-            $contentBody->product,
-            $contentBody->resource_uri
+            $price->product,
+            $price->resource_uri
         );
     }
 
-    public function createStoreBag(array $contentBody) : Bag
+    public function createStoreBag(array $stores) : Bag
     {
-        $stores = new Bag();
+        $storeBag = new Bag();
 
-        foreach($contentBody as $rawStore)
+        foreach($stores as $rawStore)
         {
             $store = $this->createStore($rawStore);
-            $stores->set($store->getId(), $store);
+            $storeBag->set($store->getId(), $store);
         }
 
-        return $stores;
+        return $storeBag;
     }
 
-    public function createStore(stdClass $contentBody) : Store
+    public function createStore(stdClass $store) : Store
     {
         return new Store(
-            $contentBody->id,
-            $contentBody->key,
-            $contentBody->name,
-            $contentBody->address,
-            $contentBody->address_raw,
-            $contentBody->phone,
-            $contentBody->county,
-            $contentBody->hours_raw,
-            $contentBody->latitude,
-            $contentBody->longitude,
-            $contentBody->resource_uri
+            $store->id,
+            $store->key,
+            $store->name,
+            $store->address,
+            $store->address_raw,
+            $store->phone,
+            $store->county,
+            $store->hours_raw,
+            $store->latitude,
+            $store->longitude,
+            $store->resource_uri
         );
     }
 
-    public function createProductBag(array $contentBody) : Bag
+    public function createProductBag(array $products) : Bag
     {
-        $products = new Bag();
+        $productBag = new Bag();
 
-        foreach($contentBody as $rawProduct) {
+        foreach($products as $rawProduct) {
             $product = $this->createProduct($rawProduct);
-            $products->set($product->getId(), $product);
+            $productBag->set($product->getId(), $product);
         }
 
-        return $products;
+        return $productBag;
     }
 
-    public function createProduct(stdClass $contentBody) : Product
+    public function createProduct(stdClass $product) : Product
     {
-        $createdAt = new DateTime($contentBody->created_at);
-        $modifiedAt = new DateTime($contentBody->modified_at);
+        $createdAt = new DateTime($product->created_at);
+        $modifiedAt = new DateTime($product->modified_at);
 
         $product = new Product(
-            $contentBody->id,
-            $contentBody->title,
-            $contentBody->age,
-            $contentBody->bottles_per_case,
-            $contentBody->description,
-            $contentBody->on_sale,
-            $contentBody->proof,
-            $contentBody->size,
-            $contentBody->code,
-            $contentBody->slug,
+            $product->id,
+            $product->title,
+            $product->age,
+            $product->bottles_per_case,
+            $product->description,
+            $product->on_sale,
+            $product->proof,
+            $product->size,
+            $product->code,
+            $product->slug,
             $createdAt,
             $modifiedAt,
-            $contentBody->resource_uri
+            $product->resource_uri
         );
 
-        $prices = $this->api->getPrices(['product' => $contentBody->id]);
+        $prices = $this->api->getPrices(['product' => $product->id]);
         $product->setPrices($prices);
         
         return $product;
