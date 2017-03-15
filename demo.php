aea9910dce0a0d126d0079e4506aa98272c09a41
David Frailey <david@snapshot.is>
2017-03-14 20:28:08 -0700

    Updated the demo file.

--- demo.php
+++ demo.php
@@ -8,7 +8,26 @@ use David\Http\Client;
 $client = new Client();
 $api = new Api($client);
 
-$product = $api->getProduct(5111);
+$product = $api->getProduct(1);
+
 print_r($product);
-$prices = $product->getPrices();
+
+$price = $api->getPrice(1);
+
+print_r($price);
+
+$store = $api->getStore(1);
+
+print_r($store);
+
+$products = $api->getProducts();
+
+print_r($products);
+
+$prices = $api->getPrices();
+
 print_r($prices);
+
+$stores = $api->getStores();
+
+print_r($stores);
